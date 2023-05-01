9c6ba480d050799e559027849e891f264a60f6ac
thegolumehra <golucodes@gmail.com>
2023-05-01 15:57:45 +0530

    init with cra

---
+++ src/components/PreNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/PreNavbar.css';
+
+const PreNavbar = () => {
+  return (
+    <>
+    <div className="prenav">this is div tag</div>
+    </>
+  );
+};
+
+export default PreNavbar;
